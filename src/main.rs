d2b60d78cd86e4d4d72a31710c327408fa4fb18e
Joshix <joshix.dev@mailbox.org>
2025-01-25 16:00:00 +0000

    add get_path_with_fallbacks fn

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 mod process_tree;
 
 use std::{
+    ffi::OsString,
     io::{stdout, Write},
     path::PathBuf,
 };
@@ -22,19 +23,22 @@ fn get_path() -> ProcResult<PathBuf> {
     t.into_deepest_leaf().map(|proc| proc.into_cwd())
 }
 
-fn main() -> std::io::Result<()> {
-    let path = match get_path() {
-        Ok(path) => path.into(),
-        Err(error) => {
-            eprintln!("Could not get cwd: {error}");
-            if let Some(home) = std::env::var_os("HOME") {
-                home
-            } else {
-                eprintln!("HOME not set");
-                "/".into()
-            }
-        }
+fn get_path_with_fallbacks() -> OsString {
+    match get_path() {
+        Ok(path) => return path.into(),
+        Err(error) => eprintln!("Could not get cwd: {error}"),
     };
+
+    match std::env::var_os("HOME") {
+        Some(home) => return home,
+        None => eprintln!("HOME not set"),
+    }
+
+    "/".into()
+}
+
+fn main() -> std::io::Result<()> {
+    let path = get_path_with_fallbacks();
     stdout().write_all(path.as_encoded_bytes())?;
     println!();
     Ok(())
