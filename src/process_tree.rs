7e949791a175b25e82802ff182e36c937fc6fe72
Joshix <joshix.dev@mailbox.org>
2025-02-06 19:00:00 +0000

    read all tasks

--- src/process_tree.rs
+++ src/process_tree.rs
@@ -39,15 +39,6 @@ impl Process {
         Ok(Process { proc })
     }
 
-    fn children(&self) -> ProcResult<impl IntoIterator<Item = ProcResult<Process>>> {
-        Ok(self
-            .proc
-            .task_main_thread()?
-            .children()?
-            .into_iter()
-            .map(Process::new))
-    }
-
     pub fn is_tty(&self) -> bool {
         match self.proc.stat() {
             Ok(stat) => stat.tty_nr != 0,
@@ -56,32 +47,49 @@ impl Process {
     }
 
     #[inline]
-    fn check_children(
+    fn add_children_to_stack(
+        &self,
+        depth: usize,
+        stack: &mut Vec<(usize, ProcResult<Process>)>,
+    ) -> ProcResult<()> {
+        for task in self.tasks()? {
+            stack.extend(
+                task?
+                    .children()?
+                    .into_iter()
+                    .map(|pid| (depth, Process::new(pid))),
+            );
+        }
+
+        Ok(())
+    }
+
+    #[inline]
+    fn find_deepest_leaf(
         self,
         depth: usize,
         stack: &mut Vec<(usize, ProcResult<Process>)>,
-        deapest_leaf: &mut Option<(usize, ProcResult<CwdProcess>)>,
+        deepest_leaf: &mut Option<(usize, ProcResult<CwdProcess>)>,
     ) {
         if !self.is_tty() {
             // this can happen when forking of a terminal
             // e.g. some random deeply-nested gcc process
             // we only want to get cwds of procs with tty
-            eprintln!("Ignoring process {} (not tty)", self.proc.pid());
+            eprintln!("Ignoring process {} (not tty)", self.pid());
             return;
         }
         // add the children to the stack
-        match self.children() {
-            Ok(children) => stack.extend(children.into_iter().map(|proc| (depth + 1, proc))),
-            Err(err) => eprintln!("Could not go deeper: {err}"),
+        if let Err(err) = self.add_children_to_stack(depth + 1, stack) {
+            eprintln!("Error while getting children of {}: {err}", self.pid());
         }
         // the current maximum depth
-        let max_depth = deapest_leaf.as_ref().map(|(d, _)| *d).unwrap_or(0);
+        let max_depth = deepest_leaf.as_ref().map(|(d, _)| *d).unwrap_or(0);
         if depth <= max_depth {
             // depth isn't greater than max_depth
             // self is not a leaf, can be ignored
             return;
         }
-        let max_is_ok = deapest_leaf
+        let max_is_ok = deepest_leaf
             .as_ref()
             .map(|(_, r)| r.is_ok())
             .unwrap_or(false);
@@ -90,30 +98,28 @@ impl Process {
         let cwd: ProcResult<_> = self.try_into();
         if cwd.is_ok() || !max_is_ok {
             // could read cwd or max wasn't ok
-            *deapest_leaf = Some((depth, cwd));
+            *deepest_leaf = Some((depth, cwd));
         }
     }
 
     pub fn into_deepest_leaf(self) -> ProcResult<CwdProcess> {
-        let mut stack: Vec<(usize, ProcResult<Process>)> = match self.children() {
-            Ok(children) => children.into_iter().map(|proc| (1, proc)).collect(),
-            Err(error) => {
-                eprintln!("Could not get children: {error}");
-                // return self as we couldn't get the children
-                return self.try_into();
-            }
+        let mut stack: Vec<(usize, ProcResult<Process>)> = vec![];
+        if let Err(error) = self.add_children_to_stack(1, &mut stack) {
+            eprintln!("Could not get children: {error}");
+            // return self as we couldn't get the children
+            return self.try_into();
         };
         // the cwd of the process with the maximum depth
-        let mut deapest_leaf: Option<(usize, ProcResult<CwdProcess>)> = None;
+        let mut deepest_leaf: Option<(usize, ProcResult<CwdProcess>)> = None;
         // loop over all the children in the stack
         while let Some((depth, child)) = stack.pop() {
             match child {
-                Ok(child) => child.check_children(depth, &mut stack, &mut deapest_leaf),
+                Ok(child) => child.find_deepest_leaf(depth, &mut stack, &mut deepest_leaf),
                 Err(err) => eprintln!("Could not get child: {err}"),
             }
         }
         // return the cwd ProcResult with the maximum depth or fallback to self
-        deapest_leaf
+        deepest_leaf
             .map(|(_, cwd_result)| cwd_result)
             .unwrap_or_else(|| self.try_into())
     }
