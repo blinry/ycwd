07209dc542c6b917c72b00e5e1d55af9b10666ed
Joshix <joshix.dev@mailbox.org>
2025-03-03 16:00:00 +0000

    improve tests

--- scripts/set_cwd.sh
+++ scripts/set_cwd.sh
@@ -5,8 +5,16 @@ set -eu
 CWD="$1"
 COUNTER="${2:-50}"
 
-if [ "$COUNTER" -eq "0" ]
+if [ "$COUNTER" -eq "-10" ]
 then
+    cd "$CWD"
+    sleep 10
+elif [ "$COUNTER" -eq "0" ]
+then
+    # background processes that should get ignored
+    setsid "$0" "${TMPDIR:-/tmp}" "-1" &
+
+    # go to dir
     cd "$CWD"
     sleep 10
 elif [ "$COUNTER" -eq "30" ]
--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,12 @@ cd "${SCRIPTS_DIR}"
 SCRIPTS_DIR="$(pwd)"
 cd - > /dev/null
 
+EXIT_CODE=0
+
 for file in "${SCRIPTS_DIR}"/dirs/*
 do
-    "${SCRIPTS_DIR}/test_path.sh" "${file}"
+    echo + "${SCRIPTS_DIR}/test_path.sh" "${file}" >&2
+    "${SCRIPTS_DIR}/test_path.sh" "${file}" || EXIT_CODE=1
 done
+
+exit "$EXIT_CODE"
